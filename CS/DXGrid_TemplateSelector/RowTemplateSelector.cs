d7a8314590d06978f28f9b18b41ee07b370a5ceb
AndreySlabov <andrey.slabov@devexpress.com>
2022-06-22 17:23:32 +0400

    Update sample

--- CS/DXGrid_TemplateSelector/RowTemplateSelector.cs
+++ CS/DXGrid_TemplateSelector/RowTemplateSelector.cs
@@ -1,13 +1,9 @@
 ﻿using DevExpress.Xpf.Grid;
-using System;
-using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 
-namespace DXGrid_TemplateSelector
-{
-    public class RowTemplateSelector : DataTemplateSelector
-    {
+namespace DXGrid_TemplateSelector {
+    public class RowTemplateSelector : DataTemplateSelector {
         public DataTemplate EvenRowTemplate { get; set; }
         public DataTemplate OddRowTemplate { get; set; }
         public override DataTemplate SelectTemplate(object item, DependencyObject container) {
--- CS/DXGrid_TemplateSelector/Window1.xaml.cs
+++ CS/DXGrid_TemplateSelector/Window1.xaml.cs
@@ -1,21 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.ObjectModel;
 using System.Windows;
 
 namespace DXGrid_TemplateSelector {
-
     public partial class Window1 : Window {
         public Window1() {
             InitializeComponent();
             grid.ItemsSource = IssueDataObject.GetData();
         }
     }
-
     public class IssueDataObject {
         public string IssueName { get; set; }
-        public static List<IssueDataObject> GetData() {
-            List<IssueDataObject> data = new List<IssueDataObject>();
+        public static ObservableCollection<IssueDataObject> GetData() {
+            ObservableCollection<IssueDataObject> data = new ObservableCollection<IssueDataObject>();
             data.Add(new IssueDataObject() { IssueName = "Transaction History" });
             data.Add(new IssueDataObject() { IssueName = "Ledger: Inconsistency" });
             data.Add(new IssueDataObject() { IssueName = "Data Import" });
